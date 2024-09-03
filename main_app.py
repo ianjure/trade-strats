ee6a8b7d6954edc911dcb513dc9778c74b15679e
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-03 10:26:47 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -5,3 +5,6 @@ st.title("Main Menu")
 main = st.Page(page="main_app.py", title="Home", icon="🏠", default=True),
 markov = st.Page(page="markov/markov_app.py", title="Markov", icon="1️⃣"),
 meanrev = st.Page(page="mean-reversion/meanrev_app.py", title="Mean Reversion", icon="🔥")
+
+pg = st.navigation(pages=[main, markov, meanrev])
+pg.run()
