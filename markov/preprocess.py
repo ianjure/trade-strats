3bae8d27d54e6df00dc2ed37c3c210a0e7898e24
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-03 19:39:15 +0800

    Update preprocess.py

--- markov/preprocess.py
+++ markov/preprocess.py
@@ -1,1 +1,21 @@
+import pandas as pd
+import numpy as np
 
+def preprocess(stock):
+    # Drop unnecessary columns
+    del stock["Dividends"]
+    del stock["Stock Splits"]
+
+    # Convert the index to a datetime object
+    stock.index = stock.index.strftime('%Y-%m-%d')
+    stock.index = pd.to_datetime(stock.index)
+
+    # Calculate daily returns
+    stock["Daily Returns"] = stock["Close"].pct_change()
+
+    # Create the current and future state columns
+    conditions = [stock['Daily Returns'] > 0.005, stock['Daily Returns'] < 0]
+    stock["CurrentState"] = np.select(conditions, ["Up", "Down"], default="Neutral")
+    stock["FutureState"] = stock["CurrentState"].shift(-1)
+
+    return stock
