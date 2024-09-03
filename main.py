d40d4a19f009ffc31aea637bde0b10242e302e69
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-03 10:46:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import streamlit as st
 
-st.title("Main Menu")
+# --- PAGE SETUP ---
+home_page = st.Page(
+    "home/home.py",
+    title="Home",
+    icon=":material/account_circle:",
+    default=True,
+)
+markov_page = st.Page(
+    "markov/markov_app.py",
+    title="Markov",
+    icon=":material/bar_chart:",
+)
+meanrev_page = st.Page(
+    "mean-reversion/meanrev_app.py",
+    title="Mean Reversion",
+    icon=":material/smart_toy:",
+)
 
-pages = {
-    "Info": [
-        st.Page("home/home.py", title="Home", icon="🏠")
-    ],
-    "Strategies": [
-        st.Page("markov/markov_app.py", title="Markov", icon="1️⃣"),
-        st.Page("mean-reversion/meanrev_app.py", title="Mean Reversion", icon="🔥")
-    ],
-}
+# --- NAVIGATION SETUP [WITH SECTIONS]---
+pg = st.navigation(
+    {
+        "Info": [home_page],
+        "Strategies": [markov_page, meanrev_page],
+    }
+)
 
-pg = st.navigation(pages)
+# --- RUN NAVIGATION ---
 pg.run()
