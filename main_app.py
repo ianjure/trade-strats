229aaf2ce9495912ee959cc60a4fc7e1798bf66e
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-03 10:07:45 +0800

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header("Main Menu")
+
+pg = st.navigation([
+    st.Page("main_app.py", title="Main Menu", icon="🔥"),
+    st.Page("markov/markov_app.py", title="Markov", icon="🔥"),
+    st.Page("mean-reversion/meanrev_app.py", title="Mean Reversion", icon="🔥")
+])
+pg.run()
