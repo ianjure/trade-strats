85e28505caabdaee9d5d9ce3794438a27a447e33
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-04 10:47:13 +0800

    Update about.py

--- about.py
+++ about.py
@@ -9,7 +9,7 @@ hide = """
 st.markdown(hide, unsafe_allow_html=True)
 
 # TITLE
-st.markdown("<p style='text-align: center; font-size: 3.4rem; font-weight: 800; line-height: 0.8;'>TradeStrats</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; font-size: 3.4rem; font-weight: 800; line-height: 0.8;'>Tradestrats</p>", unsafe_allow_html=True)
 
 # SUBTITLE
 st.markdown("<p style='text-align: center; font-size: 1rem; font-weight: 500; line-height: 1.2;'>A platform for testing trading algorithms, built with Streamlit.</p>", unsafe_allow_html=True)
