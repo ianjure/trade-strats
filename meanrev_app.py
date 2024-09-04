8b9ee975de4efa88254a6d495dc2708a2592cb31
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-04 10:17:31 +0800

    Update meanrev_app.py

--- meanrev_app.py
+++ meanrev_app.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
-st.title("Mean Reversion")
+# TITLE
+st.markdown("<p style='text-align: center; font-size: 3.4rem; font-weight: 800; line-height: 0.8;'>Mean Reversion</p>", unsafe_allow_html=True)
