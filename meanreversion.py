58b18338329b8554475e2d53a27ca25915de4186
Ian Jure Macalisang <ianjuremacalisang2@gmail.com>
2024-09-05 11:29:42 +0800

    Update and rename meanrev_app.py to meanreversion.py

--- meanreversion.py
+++ meanreversion.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from meanreversion_functions import preprocess, run_simulation
 
 # TITLE
 st.markdown("<p style='text-align: center; font-size: 3.4rem; font-weight: 800; line-height: 0.8;'>Mean Reversion</p>", unsafe_allow_html=True)
